5f16c16a24cd0fcd9494f40b73166f6b8fd95ec5
John Sorensen <john.sorensen@usda.gov>
2025-01-10 16:25:18 -0600

    Added instructions.py

---
+++ src/instructions.py
@@ -0,0 +1,239 @@
+class skalar_recipes:
+
+    rinsingLiquid = {
+        "500 mL" : "Add 1 mL of concentrated Sulfuric Acid to 500 mL of Deionized Water",
+        "1 L": "Add 2 mL of concentrated Sulfuric Acid to 1 L of Deionized Water",
+        "2 L": "Add 4 mL of concentrated Sulfuric Acid to 2 L of Deionized Water"
+    }
+
+    nitrateNitriteBuffer = {
+        "500 mL" : """Fill a 500 mL volumetric flask half full with Deionized Water\n
+                    add 25 g of Ammonium Chlorde\n
+                    Add 3.6 mL of ammonium Hydroxide\n
+                    Dilute to mark.""",
+                            
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+                Add 50 g of Ammonium Chloride\n
+                Add 7.2 mL of Ammonium Hydroxide\n
+                Dilute to mark.""",
+        "2 L" : """Fill a 2000 mL volumetric flask half full with Deionized Water\n
+                Add 100 g of Ammonium Chloride\n
+                Add 14.4 mL of Ammonium Hydroxide\n
+                Dilute to mark."""
+    }
+
+    nitrateNitriteColor = {
+        "500 mL" : """Fill a 500 mL volumetric flask half full with Deionized Water\n
+        Add 50 mL of 32% Hydrochloric Acid\n
+        Add 0.25 g of N-(1Naphtyl)ethylenediamine dihydrochloride\n
+        Add 5 g of Sulfanilamide\n
+        Dilute to mark.""",
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 100 mL of 32% Hydrochloric Acid\n
+        Add 0.5 g of N-(1Naphtyl)ethylenediamine dihydrochloride\n
+        Add 10 g of Sulfanilamide\n
+        Dilute to mark."""
+    }
+
+    phosphateFFD6 = {
+        "500 mL" : """To 500 mL of Deionized Water add 1 mL of FFD6.""",
+        "1 L" : """To 1000 mL of Deionized Water add 2 mL of FFD6."""
+    }
+    phosphateSulfuric = {
+        "500 mL" : """Fill a 500 mL volumetric flask half full with Deionized Water\n
+        Add 20 mL of concentrated Sulfuric Acid\n
+        Dilute to mark, pour into reagent bottle\n
+        Add 1 mL of FFD6 to bottle.""",
+        "1 L" : """Fill 1000 mL volumetric flask half full with Deionized Water\n
+        Add 40 mL of concentrated Sulfuric Acid\n
+        Dilute to mark, pour into reagent bottle\n
+        Add 2 mL of FFD6 to bottle."""
+    }
+
+    phosphateAmmoniumHeptamolybdate = {
+        "500 mL" : """Fill a 500 mL volumetric flask half full with Deionized Water\n
+        Add 20 mL of concentrated Sulfuric Acid\n
+        Add 2.4 g of Ammoniumheptamolybdatate\n
+        Dilute to mark, pour into reagent bottle\n
+        Add 1 mL of FFD6 to bottle.""",
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 40 mL of concentrated Sulfuric Acid\n
+        Add 4.8 g of Ammonium Heptamolybdate\n
+        Dilute to mark, pour into reagent bottle\n
+        Add 2 mL of FFD6 to bottle."""
+    }
+
+    phosphatePotassiumAntimony = {
+        "100 mL" : """Fill a 100 mL volumetric flask half full with Deionized Water\n
+        Add 300 mg of Potassium Antimony Oxide Tartate\n
+        Dilute to mark."""
+    }
+
+    phosphateLAscorbicAcid = {
+        "500 mL" : """Fill a 500 mL volumetric flask half full with Deionized Water\n
+        Add 9 g of L+ Ascorbic Acid\n
+        Add 10 mL of Potasssium Antimony Stock Solution\n
+        Dilute to mark.""",
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 18 g of L+ Ascorbic Acid\n
+        Add 20 mL of Potassium Antimony Stock Solution\n
+        Dilute to mark."""
+    }
+
+    ammoniaBuffer = {
+        "500 mL" : """Fill a 500 mL volumetric flask half full with Deionized Water\n
+        Add 16.5 g of Potassium Sodium Tartate\n
+        Add 12 g of Sodium Citrate Dihydrate\n
+        Add 3 mL of 32% Hydrochloric Acid or 5 mL of 6N Hydrochloric Acid\n
+        Dilute to mark.""",
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 33 g of Potassium Sodium Tartate\n
+        Add 24 g of Sodium Citrate Dihydrate\n
+        Add 6 mL of 32% Hydrochloric Acid or 10 mL of 6N Hydrochloric Acid\n
+        Dilute to mark.""",
+        "2 L" : """Fill a 2000 mL volumetric flask half full with Deionized Water\n
+        Add 66 g of Potassium Sodium Tartate\n
+        Add 48 g of Sodium Citrate Dihydrate\n
+        Add 12 mL of 32% Hydrochloric acid or 20 mL of 6N Hydrochloric Acid\n
+        Dilute to mark."""
+    }
+    ammoniaSalicyalite = {
+        "500 mL" : """Fill a 500 mL volumetric falsk half full with Deionized Water\n
+        Add 12.5 g of Sodium Hydroxide\n
+        Add 40 g of Sodium Salicyalite\n
+        Dilute to mark.""",
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 25 g of Sodium Hydroxide\n
+        Add 80 g of Sodium Salicyalite\n
+        Dilute to mark."""
+    }
+
+    ammoniaNitroprusside = {
+        "500 mL" : """Fill a 500 mL volumetric flask half full with Deionized Water\n
+        Add 0.5 g of Sodium Nitroprusside\n
+        Dilute to mark.""",
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 0.5 g of Sodium Nitroprusside\n
+        Dilute to mark."""
+    }
+
+    ammoniaDichlorocyanurite = {
+        "500 mL" : """Fill a 500 ml volumetric flask half full with Deionized Water\n
+        Add 1.0 g of Sodium Dichlorocyanurite\n
+        Dilute to mark.""",
+        "1 L" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 2.0 g of Sodium Dichlorocyanurite\n
+        Dilute to mark."""
+    }
+
+    skalarSTD = {
+        "STD 1" : """Add 250 uL of 100 ppm Nitrate/Nitrite Stock Standard\n
+        Add 25 uL of 1000 ppm Phosphate Stock Standard\n
+        Add 25 uL of 1000 ppm Ammonia Stock Standard\n
+        Dilute to 100 mL for final 0.25 ppm""",
+        "STD 2" : """Add 750 uL of 100 ppm Nitrate/Nitrite Stock Standard\n
+        Add 75 uL of 1000 ppm Phosphate Stock Standard\n
+        Add 75 uL of 1000 ppm Ammonia Stock Standard\n
+        Dilute to 100 mL for final 0.75 ppm""",
+        "STD 3" : """Add 1000 uL of 100 ppm Nitrate/Nitrite Stock Standard\n
+        Add 100 uL of 1000 ppm Phosphate Stock Standard\n
+        Add 100 uL of 1000 ppm Ammonia Stock Standard\n
+        Dilute to 100 mL for final 1.0 ppm""",
+        "STD 4" : """Add 2000 uL of 100 ppm Nitrate/Nitrite Stock Standard\n
+        Add 200 uL of 1000 ppm Phosphate Stock Standard\n
+        Add 200 uL of 1000 ppm Ammonia Stock Standard\n
+        Dilute to 100 mL for final 2.0 ppm""",
+        "STD 5" : """Add 4000 uL of 100 ppm Nitrate/Nitrite Stock Standard\n
+        Add 400 uL of 1000 ppm Phosphate Stock Standard\n
+        Add 400 uL of 1000 ppm Ammonia Stock Standard\n
+        Dilute to 100 mL for final 4.0 ppm"""
+    }
+
+class extractant_recipes:
+
+    mehlich3Stock = """Fill a 100 mL volumetric flask with 50-60 mL of Deionized Water\n
+    Add 5.556 g of Ammonium FLouride\n
+    Add 2.823 g of Ethlenediaminetetraacetic acid\n
+    Stir on hotplate until dissolved\n
+    Dilute to mark."""
+
+    mehlich3 = {
+        "2.5 L" : """Fill a 1000 mL volumetric flask half full of Deionized Water\n
+        Add 50.025 g of Ammonium Nitrate\n
+        Add 25 mL of Stock Solution\n
+        Add 28.75 mL of Glacial Acetic Acid\n
+        Add 20.5 mL of 10% Nitric Acid\n
+        Stir on hotplate until dissolved\n
+        Dilute to mark\n
+        Transfer to solution container and add 1.5 L of Deionized Water.""",
+        "5.0 L" : """Fill a 2000 mL volumetric falsk with 700-800 mL of Deionized Water\n
+        Add 100.05 g of Ammonium Nitrate\n
+        Add 50 mL of Stock Solution\n
+        Add 57.5 mL of Glacial Acetic Acid\n
+        Add 41 mL of 10% Nitric Aci\n
+        Stir on hotplate until dissolved\n
+        Dilute to mark\n
+        Transfer to solution container and add 3 L of Deionized Water."""
+    }
+
+    H3A = {
+        "1 L" : """Fill a 1000 mL volumetric flask half full of Deionzied Water\n
+        Add 1.1 g of Malic Acid\n
+        Add 0.7 g of Citric Acid\n
+        Add 0.45 g of Oxalic Acid\n
+        Stir on hotplate until dissolved\n
+        Dilute to mark.""",
+        "2 L" : """Fill a 2000 mL volumetric flask half full mL of Deionized Water\n
+        Add 2.2 g of Malic Acid\n
+        Add 1.4 g of Citric Acid\n
+        Add 0.90 g of Oxalic Acid\n
+        Stir on hotplate until dissolved\n
+        Dilute to mark."""
+    }
+
+    olsenP = {
+        "1 L" : """FIll a 1000 mL volumetric flask half full of Deionized Water\n
+        Add 42.0 g of Sodium Carbonate\n
+        Stir on hotplate until dissolved\n
+        Check pH and add Sodium Hydroxide as needed until solution pH is 8.0\n
+        Use Hydrochloric Acid if pH is too high\n
+        Dilute to mark.""",
+        "2 L" : """Fill a 2000 mL volumetric flask half full mL of Deionized Water\n
+        Add 84.0 g of Sodium Carbonate\n
+        Stir on hotplate until dissolved\n
+        Check pH and add Sodium Hydroxide as needed until solution pH is 8.0\n
+        Use Hydrochloric Acid if pH is too high\n
+        Dilute to mark"""
+    }
+
+    potassiumChloride = {
+        "1 L 1 N" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 74.55 g of Potassium Chloride\n
+        Stir on hotplate until dissolved\n
+        Dilute to mark.""",
+        "2 L 1 N" : """Fill a 2000 mL volumetric flask half full with Deionized Water\n
+        Add 149.10 g of Potassium Chloride\n
+        Stir on hotplate until dissolved\n
+        Dilute to mark.""",
+        "1 L 2 N" : """Fill a 1000 mL volumetric flask half full with Deionized Water\n
+        Add 149.10 g of Potassium Chloride\n
+        Stir on hotplate until dissolved\n
+        Dilute to mark.""",
+        "2 L 2 N" : """Fill a 2000 mL volumetric flask half full with Deionized Water\n
+        Add 298.2 g of Potassium Chloride\n
+        Stir on hotplate until disssolved\n
+        Dilute to mark."""
+    }
+class icp_recipes:
+
+    icpSTD = {
+        "STD 3" : """Standard 3 is a 1:2 serial dilution of STD 4 with Deionized Water\n
+        Add 25 mL of STD 4 to a 50 mL volumetric flask\n
+        Dilute to mark.""",
+        "STD 2" : """Standard 2 is a 1:2 serial dilution of STD 3 with Deionized Water\n
+        Add 25 mL of STD 3 to a 50 mL volumetric flask\n
+        Dilute to mark.""",
+        "STD 1" : """Standard 1 is a 1:10 serial dilution of STD 2 with Deionized Water\n
+        Add 5 mL of STD 2 to a 50 mL volumetric flask\n
+        Dilute to mark."""
+    }
