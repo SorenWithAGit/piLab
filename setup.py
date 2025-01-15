7490287b2947e0b126643adfb92ddcdbb96dee82
John Sorensen <john.sorensen@usda.gov>
2025-01-15 15:57:02 -0600

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='readInstruments_js',
+    version='1.0',
+    url='https://github.com/SorenWithAGit/piLab.git',
+    license='CC0 1.0',
+    author='John A Sorensen',
+    author_email='john.sorensen@usda.gov',
+    description='A GUI for the Labs at GSWRL Temple, TX.'
+    long_description = file: README.md
+    long_description_content_type = text/markdown
+)
