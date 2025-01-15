6a828caf5ed85ad13185db4cc0e9addb54a15c88
John Sorensen <john.sorensen@usda.gov>
2025-01-15 14:16:18 -0600

    Added standards to gui for ghg

--- src/ghg.py
+++ src/ghg.py
@@ -6,32 +6,39 @@ class ghg_window:
     def steps():
         step_frame = tk.Frame()
         step_frame.pack()
+        stds_click = tk.Button(step_frame,
+                               text = "GHG Standards",
+                               font = ("Arial", 20),
+                               command = lambda: [mw.window.clear_frame(step_frame),
+                                                  operating_procedure.stds()]).grid(row = 0, column = 0,
+                                                                                    padx = 20, 
+                                                                                    pady = 20)
         step1_click = tk.Button(step_frame,
                                 text = "Step 1",
                                 font = ("Arial", 20),
                                 command = lambda: [mw.window.clear_frame(step_frame),
-                                                   operating_procedure.step1()]).grid(row = 0, column = 0,
+                                                   operating_procedure.step1()]).grid(row = 0, column = 1,
                                                                                 padx = 20,
                                                                                 pady = 20)
         step2_click = tk.Button(step_frame,
                                 text = "Step 2",
                                 font = ("Arial", 20),
                                 command = lambda: [mw.window.clear_frame(step_frame),
-                                                   operating_procedure.step2()]).grid(row = 0, column = 1,
+                                                   operating_procedure.step2()]).grid(row = 0, column = 2,
                                                                                 padx = 20,
                                                                                 pady = 20)
         step3_click = tk.Button(step_frame,
                                 text = "Step 3",
                                 font = ("Arial", 20),
                                 command = lambda: [mw.window.clear_frame(step_frame),
-                                                   operating_procedure.step3()]).grid(row = 0, column = 2,
+                                                   operating_procedure.step3()]).grid(row = 1, column = 0,
                                                                                 padx = 20,
                                                                                 pady = 20)
         step4_click = tk.Button(step_frame,
                                 text = "Step 4",
                                 font = ("Arial", 20),
                                 command = lambda: [mw.window.clear_frame(step_frame),
-                                                   operating_procedure.step4()]).grid(row = 1, column = 0,
+                                                   operating_procedure.step4()]).grid(row = 1, column = 1,
                                                                                 padx = 20,
                                                                                 pady = 20)
         smtih_home_button = tk.Button(step_frame,
@@ -42,6 +49,34 @@ class ghg_window:
                                                                         column = 0,
                                                                         pady = 20)
 class operating_procedure:
+
+    def stds():
+        instruct_frame = tk.Frame()
+        instruct_frame.pack()
+        stds = ins.ghg_stds
+        text = tk.Text(instruct_frame,
+                           height = 13,
+                           width = 48,
+                           font = ("Arial", 20))
+        text.grid(row = 0, column = 1)
+        step = stds.stds
+        text.insert(tk.END, step)
+
+        smith_home_button = tk.Button(instruct_frame,
+                                font = ("Arial", 20),
+                                text = "Smith Lab Home",
+                                command = lambda: [mw.window.clear_frame(instruct_frame),
+                                    mw.window.Smith_lab_home()]).grid(row = 1, column = 0,
+                                                                      padx = 20,
+                                                                      pady = 20)
+        step_home_button = tk.Button(instruct_frame,
+                                     font = ("Arial", 20),
+                                     text = "Skalar Step Home",
+                                     command = lambda: [mw.window.clear_frame(instruct_frame),
+                                                        ghg_window.steps()]).grid(row = 1, column = 1,
+                                                                                      padx = 20,
+                                                                                      pady = 20)
+
     def step1():
         instruct_frame = tk.Frame()
         instruct_frame.pack()
