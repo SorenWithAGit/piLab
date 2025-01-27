55189c3bef3f7547348d5324d79c65877a0d85e1
John Sorensen <john.sorensen@usda.gov>
2025-01-27 13:50:34 -0600

    Restructured MainGUI

--- main.py
+++ main.py
@@ -3,50 +3,53 @@
 import tkinter as tk
 from src.smithLab import procedure as pr
 
-class MainWindow(tk.Frame):
 
+class MainGUI(tk.Tk):
     def __init__(self):
+        # Main Setup
         super().__init__()
-        self.initUI()
+        self.title("piLab By John Sorensen")
+        self.geometry("1280x800")
+        self.resizable(width = False, height = False)
 
-    def initUI(self):
-        self.master.title("piLab By John Sorensen")
+
+        # Ribbon Menu
+        piLab_menu = tk.Menu(self)
+        self.config(menu = piLab_menu)
+
+        home_menu = tk.Menu(piLab_menu)
+        piLab_menu.add_cascade(label = "Home", menu = home_menu)
+        home_menu.add_command(label = "piLab Home", command = lambda: self.temp_command())
+        home_menu.add_command(label = "Exit", command = self.quit)
+    
+        smith_menu  = tk.Menu(piLab_menu)
+        piLab_menu.add_cascade(label = "Smith Lab", menu = smith_menu)
+        smith_menu.add_command(label = "Smith Lab Home", command = lambda: self.temp_command())
+        smith_menu.add_separator()
+        smith_menu.add_command(label = "Extraction Recipes", command = lambda: self.temp_command())
+        smith_menu.add_command(label = "Instrument Recipes", command = lambda: self.temp_command())
+        smith_menu.add_command(label = "Extraction Protocols", command = lambda: self.temp_command())
+        smith_menu.add_command(label = "Analytical Instrumentation", command = lambda: self.temp_command())
+
+        yost_menu = tk.Menu(piLab_menu)
+        piLab_menu.add_cascade(label = "Yost Lab", menu = yost_menu)
+
+        schantz_menu = tk.Menu(piLab_menu)
+        piLab_menu.add_cascade(label = "Schantz Lab", menu = schantz_menu)
+
+
+        # Home Frame
+        self.home = homeFrame(self)
+
+        # Run
+        self.mainloop()
+
+    def temp_command():
+        pass
+
+class homeFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
         pr.mainFrames.piLab_home()
 
-def temp_command():
-     pass
-
-def main():
-    root = tk.Tk()
-    root.title("piLab By John Sorensen")
-    root.geometry("1280x800")
-    root.resizable(width = False, height = False)
-
-    piLab_menu = tk.Menu(root)
-    root.config(menu = piLab_menu)
-
-    home_menu = tk.Menu(piLab_menu)
-    piLab_menu.add_cascade(label = "Home", menu = home_menu)
-    home_menu.add_command(label = "piLab Home", command = lambda: temp_command())
-    home_menu.add_command(label = "Exit", command = root.quit)
-   
-    smith_menu  = tk.Menu(piLab_menu)
-    piLab_menu.add_cascade(label = "Smith Lab", menu = smith_menu)
-    smith_menu.add_command(label = "Smith Lab Home", command = lambda: temp_command())
-    smith_menu.add_separator()
-    smith_menu.add_command(label = "Extraction Recipes", command = lambda: temp_command())
-    smith_menu.add_command(label = "Instrument Recipes", command = lambda: temp_command())
-    smith_menu.add_command(label = "Extraction Protocols", command = lambda: temp_command())
-    smith_menu.add_command(label = "Analytical Instrumentation", command = lambda: temp_command())
-
-    yost_menu = tk.Menu(piLab_menu)
-    piLab_menu.add_cascade(label = "Yost Lab", menu = yost_menu)
-
-    schantz_menu = tk.Menu(piLab_menu)
-    piLab_menu.add_cascade(label = "Schantz Lab", menu = schantz_menu)
-
-    app = MainWindow()
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+MainGUI()
