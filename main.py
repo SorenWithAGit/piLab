8ad26e454aef295dadf3b0b5aa6d30c6101de1eb
John Sorensen <john.sorensen@usda.gov>
2025-01-28 10:16:20 -0600

    Adjusted font of menubar and seperator

--- main.py
+++ main.py
@@ -21,34 +21,40 @@ class MainGUI(tk.Tk):
         home_menu = tk.Menu(piLab_menu, tearoff = 0)
         piLab_menu.add_cascade(label = "Home", menu = home_menu)
         home_menu.add_command(label = "piLab Home",
-                              font = ("Arial", 14),
+                              font = ("Arial", 24),
                               command = lambda: [self.clear(),
-                                                                       pr.mainFrames.piLab_home()])
+                                                 pr.mainFrames.piLab_home()])
+        home_menu.add_separator()
         home_menu.add_command(label = "Exit", command = self.quit)
     
         smith_menu  = tk.Menu(piLab_menu, tearoff = 0)
         piLab_menu.add_cascade(label = "Smith Lab", menu = smith_menu)
+        smith_menu.add_separator()
         smith_menu.add_command(label = "Smith Lab Home",
-                               font = ("Arial", 14),
+                               font = ("Arial", 24),
                                command = lambda: [self.clear(),
-                                                                            pr.mainFrames.Smith_lab_home()])
+                                                  pr.mainFrames.Smith_lab_home()])
         smith_menu.add_separator()
         smith_menu.add_command(label = "Extraction Recipes",
-                               font = ("Arial", 14),
+                               font = ("Arial", 24),
                                command = lambda: [self.clear(),
-                                                                                pr.procedure_selection.extracts_click()])
+                                                  pr.procedure_selection.extracts_click()])
+        smith_menu.add_separator()
         smith_menu.add_command(label = "Instrument Recipes",
-                               font = ("Arial", 14),
+                               font = ("Arial", 24),
                                command = lambda: [self.clear(),
-                                                                                pr.procedure_selection.analytical_instruments()])
+                                                  pr.procedure_selection.analytical_instruments()])
+        smith_menu.add_separator()
         smith_menu.add_command(label = "Extraction Protocols",
-                               font = ("Arial", 14),
+                               font = ("Arial", 24),
                                command = lambda: [self.clear(),
-                                                                                  pr.procedure_selection.extraction_protocol()])
+                                                  pr.procedure_selection.extraction_protocol()])
+        smith_menu.add_separator()
         smith_menu.add_command(label = "Analytical Instrumentation",
-                               font = ("Arial", 14),
+                               font = ("Arial", 24),
                                command = lambda: [self.clear(),
-                                                                                        pr.procedure_selection.extraction_protocol()])
+                                                  pr.procedure_selection.extraction_protocol()])
+        smith_menu.add_separator()
 
         yost_menu = tk.Menu(piLab_menu)
         piLab_menu.add_cascade(label = "Yost Lab", menu = yost_menu)
